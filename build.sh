55b8a4ed645ddf835af8efe0f2b49f567372f351
zimbatm <zimbatm@zimbatm.com>
2014-10-20 14:19:04 +0100

    golang update

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@ set -e
 cd `dirname $0`
 
 VERSION=0.8
+GO_VERSION=1.3.3
 
 # Dependendies
 apt-get update -qy
 DEBIAN_FRONTEND=noninteractive apt-get install -qy curl make libsqlite3-dev
 
-curl -qLf https://storage.googleapis.com/golang/go1.3.1.linux-amd64.tar.gz | tar xzf - -C /usr/local
+curl -qLf https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz | tar xzf - -C /usr/local
 export PATH=/usr/local/go/bin:$PATH
 
 curl -qLf https://github.com/bradfitz/camlistore/archive/${VERSION}.tar.gz | tar xzf - -C /usr/src
